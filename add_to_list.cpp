4436445e68c487bc556173dead3886e14649f955
Gabriela <gabriela.urbanczyk2000@gmail.com>
2020-10-20 18:05:30 +0200

    dodanie 3 funkcji

--- add_to_list.cpp
+++ add_to_list.cpp
@@ -1,6 +1,9 @@
 #include <filesystem>  
 #include <iostream>
 #include "head.h"
+#include <fstream>
+#include <string>
+#include <map>
 namespace fs = std::filesystem;
 
 
@@ -10,6 +13,7 @@ std::string path = "C:/Users/Gabriela/Desktop/studia/io/funkcja1/funkcja1";
 std::string ext(".cpp");
 std::string exh(".h");
 std::list<std::string>pliki;
+std::map<string, vector<string >> mapa_nazw_plikow;
 
 
 void list_of_files() {
@@ -50,4 +54,43 @@ void add_to_list()
 		cout << *it << endl;
 	}
 
+}
+
+
+void fun3()
+{
+
+	for (auto it = pliki.begin(); it != pliki.end(); it++)
+	{
+		ifstream pliki("*it");
+		while (!pliki.eof())
+		{
+			string linijka;
+			getline(pliki, linijka);
+			string szukamy = "#include";
+			size_t gdzie = linijka.find(szukamy);
+			if (!(string::npos == linijka.find("#include")))
+			{
+				gdzie += szukamy.size();
+				string nazwa_pliku = linijka.substr(gdzie);
+
+				mapa_nazw_plikow[*it].push_back(nazwa_pliku);
+
+				///map<string, vector<string>>::iterator cur;
+
+				for (auto i = mapa_nazw_plikow.begin(); i != mapa_nazw_plikow.end(); i++) {
+					cout << "polaczenia plikow cpp: " << endl;
+					if (i->first != "a.out") {
+						cout << i->first << ": ";
+						for (auto ii = i->second.begin(); ii != i->second.end(); ii++) {
+							cout << *ii << " ";
+						}
+
+					}
+
+				}
+			}
+
+		}
+	}
 }
