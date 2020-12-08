206d84619e6661780f9a09a13f7e599b636ec23f
mihaf70 <72543203+mihaf70@users.noreply.github.com>
2020-12-08 11:11:40 +0100

    Update and rename head.h to Functions.h

---
+++ Functions.h
@@ -0,0 +1,24 @@
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+
+#include <filesystem>  
+#include <iostream>
+#include<fstream>
+#include<string>
+#include<map>
+
+
+	void list_of_files();
+	void add_to_list();
+	void fun_3();
+	void relation2();
+	void relation3();
+	void relation5();
+	void file_nm();
+	void graph_making(std::map<std::string, std::vector<std::string>>l, std::string name);
+	void combined_graf_making(std::map<std::string, std::vector<std::string>>l, std::map<std::string, std::vector<std::string>>x, std::map<std::string, std::vector<std::string>>a, std::map<std::string, std::vector<std::string>>b, std::map<std::string, std::vector<std::string>>h, std::string name);
+	void graf_making_2(std::map<std::string, std::vector<std::string>>l, std::map<std::string, std::vector<std::string>>x, std::map<std::string, std::vector<std::string>>h, std::string name);
+	
+	
+
+#endif
