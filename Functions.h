9579797c420302d87778a603d57bb71c01e3414b
mihaf70 <72543203+mihaf70@users.noreply.github.com>
2021-01-14 09:00:00 +0100

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -6,8 +6,9 @@
 #include<fstream>
 #include<string>
 #include<map>
+#include<list>
 
-
+	
 	void list_of_files();
 	void add_to_list();
 	void fun_3();
@@ -15,10 +16,18 @@
 	void relation3();
 	void relation5();
 	void file_nm();
-	void graph_making(std::map<std::string, std::vector<std::string>>l, std::string name);
+	void graph_making(std::map<std::string, std::vector<std::string>>l, std::string name, int o);
 	void combined_graf_making(std::map<std::string, std::vector<std::string>>l, std::map<std::string, std::vector<std::string>>x, std::map<std::string, std::vector<std::string>>a, std::map<std::string, std::vector<std::string>>b, std::map<std::string, std::vector<std::string>>h, std::string name);
 	void graf_making_2(std::map<std::string, std::vector<std::string>>l, std::map<std::string, std::vector<std::string>>x, std::map<std::string, std::vector<std::string>>h, std::string name);
+	void param_of_graph(std::map<std::string, std::vector<std::string>>l);
+	void connections(std::map<std::string, std::vector<std::string>>a);
+	void cc();
+	struct node;
+	void push(node*& S, std::string n);
+	int check(node* S, std::string n);
 	
+	
+
 	
 
 #endif
